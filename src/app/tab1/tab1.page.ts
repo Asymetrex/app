7325a8331a957c78e1272aa1fab079f4e498a50b
FallenGravity <ethanvb55@gmail.com>
2020-03-26 16:01:38 +0200

    Implement Refresh, All Volumes, Coin Logos

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -11,29 +11,83 @@ import { AlertController } from '@ionic/angular';
 export class Tab1Page {
 
   constructor(private http: HttpClient) {
-    //Fetch Base Pair Volume
-    this.http.get('https://asymetrex.com/api').subscribe((response) => {
-      this.btc = response.btc_volume
-      this.ltc = response.ltc_volume
-      this.doge = response.doge_volume
-    });
+    setInterval(() => {
+      //Fetch Base Pair Volume
+      this.http.get('https://asymetrex.com/api').subscribe((response) => {
+        this.btc = response.btc_volume
+        this.ltc = response.ltc_volume
+        this.doge = response.doge_volume
+      });
 
-    //Fetch BITC/LTC
-    this.http.get('https://asymetrex.com/api/tickers/bitcltc').subscribe((response) => {
-      this.bitc = response.ticker.vol
-    });
+      //Fetch XAM/BTC
+      this.http.get('https://asymetrex.com/api/tickers/xambtc').subscribe((response) => {
+        this.xam = response.ticker.vol
+      });
 
-    //Fetch BITC/BTC
-    this.http.get('https://asymetrex.com/api/tickers/bitcbtc').subscribe((response) => {
-      this.bitc2 = response.ticker.vol
-    });
-  }
-  doRefresh(event) {
-    console.log('Begin async operation');
+      //Fetch AQUA/BTC
+      this.http.get('https://asymetrex.com/api/tickers/aquabtc').subscribe((response) => {
+        this.aqua = response.ticker.vol
+      });
 
-    setTimeout(() => {
-      console.log('Async operation has ended');
-      event.target.complete();
-    }, 2000);
+      //Fetch ATH/BTC
+      this.http.get('https://asymetrex.com/api/tickers/athbtc').subscribe((response) => {
+        this.ath = response.ticker.vol
+      });
+
+      //Fetch BITC/LTC
+      this.http.get('https://asymetrex.com/api/tickers/bitcltc').subscribe((response) => {
+        this.bitc = response.ticker.vol
+      });
+
+      //Fetch CX/BTC
+      this.http.get('https://asymetrex.com/api/tickers/cxbtc').subscribe((response) => {
+        this.cx = response.ticker.vol
+      });
+
+      //Fetch DOGE/BTC
+      this.http.get('https://asymetrex.com/api/tickers/dogebtc').subscribe((response) => {
+        this.doge = response.ticker.vol
+      });
+
+      //Fetch DOGE/LTC
+      this.http.get('https://asymetrex.com/api/tickers/dogeltc').subscribe((response) => {
+        this.doge2 = response.ticker.vol
+      });
+
+      //Fetch ETHO/BTC
+      this.http.get('https://asymetrex.com/api/tickers/ethobtc').subscribe((response) => {
+        this.etho = response.ticker.vol
+      });
+
+      //Fetch MON/BTC
+      this.http.get('https://asymetrex.com/api/tickers/monbtc').subscribe((response) => {
+        this.mon = response.ticker.vol
+      });
+
+      //Fetch MON/DOGE
+      this.http.get('https://asymetrex.com/api/tickers/mondoge').subscribe((response) => {
+        this.mon2 = response.ticker.vol
+      });
+
+      //Fetch PRKL/BTC
+      this.http.get('https://asymetrex.com/api/tickers/prklbtc').subscribe((response) => {
+        this.prkl = response.ticker.vol
+      });
+
+      //Fetch UBQ/BTC
+      this.http.get('https://asymetrex.com/api/tickers/ubqbtc').subscribe((response) => {
+        this.ubq = response.ticker.vol
+      });
+
+      //Fetch XERO/BTC
+      this.http.get('https://asymetrex.com/api/tickers/xerobtc').subscribe((response) => {
+        this.xero = response.ticker.vol
+      });
+
+      //Fetch EOE/DOGE
+      this.http.get('https://asymetrex.com/api/tickers/eoedoge').subscribe((response) => {
+        this.eoe = response.ticker.vol
+      });
+    }, 6000);
   }
 }
