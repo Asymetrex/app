0a972e42ceeb6448b08703721d9c581c1b82e364
FallenGravity <ethanvb55@gmail.com>
2020-03-26 17:25:31 +0200

    Repair API calls

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -21,72 +21,72 @@ export class Tab1Page {
 
       //Fetch XAM/BTC
       this.http.get('https://asymetrex.com/api/tickers/xambtc').subscribe((response) => {
-        this.xam = response.ticker.vol
+        this.xam = response.ticker.volbtc
       });
 
       //Fetch AQUA/BTC
       this.http.get('https://asymetrex.com/api/tickers/aquabtc').subscribe((response) => {
-        this.aqua = response.ticker.vol
+        this.aqua = response.ticker.volbtc
       });
 
       //Fetch ATH/BTC
       this.http.get('https://asymetrex.com/api/tickers/athbtc').subscribe((response) => {
-        this.ath = response.ticker.vol
+        this.ath = response.ticker.volbtc
       });
 
       //Fetch BITC/LTC
       this.http.get('https://asymetrex.com/api/tickers/bitcltc').subscribe((response) => {
-        this.bitc = response.ticker.vol
+        this.bitc = response.ticker.volbtc
       });
 
       //Fetch CX/BTC
       this.http.get('https://asymetrex.com/api/tickers/cxbtc').subscribe((response) => {
-        this.cx = response.ticker.vol
+        this.cx = response.ticker.volbtc
       });
 
       //Fetch DOGE/BTC
       this.http.get('https://asymetrex.com/api/tickers/dogebtc').subscribe((response) => {
-        this.doge = response.ticker.vol
+        this.doge = response.ticker.volbtc
       });
 
       //Fetch DOGE/LTC
       this.http.get('https://asymetrex.com/api/tickers/dogeltc').subscribe((response) => {
-        this.doge2 = response.ticker.vol
+        this.doge2 = response.ticker.volbtc
       });
 
       //Fetch ETHO/BTC
       this.http.get('https://asymetrex.com/api/tickers/ethobtc').subscribe((response) => {
-        this.etho = response.ticker.vol
+        this.etho = response.ticker.volbtc
       });
 
       //Fetch MON/BTC
       this.http.get('https://asymetrex.com/api/tickers/monbtc').subscribe((response) => {
-        this.mon = response.ticker.vol
+        this.mon = response.ticker.volbtc
       });
 
       //Fetch MON/DOGE
       this.http.get('https://asymetrex.com/api/tickers/mondoge').subscribe((response) => {
-        this.mon2 = response.ticker.vol
+        this.mon2 = response.ticker.volbtc
       });
 
       //Fetch PRKL/BTC
       this.http.get('https://asymetrex.com/api/tickers/prklbtc').subscribe((response) => {
-        this.prkl = response.ticker.vol
+        this.prkl = response.ticker.volbtc
       });
 
       //Fetch UBQ/BTC
       this.http.get('https://asymetrex.com/api/tickers/ubqbtc').subscribe((response) => {
-        this.ubq = response.ticker.vol
+        this.ubq = response.ticker.volbtc
       });
 
       //Fetch XERO/BTC
       this.http.get('https://asymetrex.com/api/tickers/xerobtc').subscribe((response) => {
-        this.xero = response.ticker.vol
+        this.xero = response.ticker.volbtc
       });
 
       //Fetch EOE/DOGE
       this.http.get('https://asymetrex.com/api/tickers/eoedoge').subscribe((response) => {
-        this.eoe = response.ticker.vol
+        this.eoe = response.ticker.volbtc
       });
     }, 6000);
   }
