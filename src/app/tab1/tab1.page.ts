1ba8f7053d9acd8107b1ac5980652eab53360ded
FallenGravity <ethanvb55@gmail.com>
2020-03-26 15:35:00 +0200

    Basic Home page minus api work

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,30 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    //Fetch Base Pair Volume
+    this.http.get('https://asymetrex.com/api').subscribe((response) => {
+      this.btc = response.btc_volume
+      this.ltc = response.ltc_volume
+      this.doge = response.doge_volume
+    });
 
+    //Fetch BITC/LTC
+    this.http.get('https://asymetrex.com/api/tickers/bitcltc').subscribe((response) => {
+      this.bitc = response.ticker.vol
+    });
+
+    //Fetch BITC/BTC
+    this.http.get('https://asymetrex.com/api/tickers/bitcbtc').subscribe((response) => {
+      this.bitc2 = response.ticker.vol
+    });
+  }
+  doRefresh(event) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
+  }
 }
